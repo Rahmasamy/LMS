0bd24b1cbd2c89b4c8d54720a8818199167a3e84
ahmedhussein70 <ahmed.hussein201170@gmail.com>
2024-09-20 22:40:31 +0300

    base project

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
