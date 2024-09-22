1afe96c3926983e851ca9ffa00e657ca7d4ec6fc
Rahmasamy <rahmasamy949@gmail.com>
2024-09-22 14:07:22 +0300

    home page update

---
+++ LMS/front-end/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { InstractorComponent } from './components/instractor/instractor.component';
+import { HomePageComponent } from './components/User/home-page/home-page.component';
+
+export const routes: Routes = [
+{path: '', component:HomePageComponent},
+{path: 'instructor', component:InstractorComponent},
+];
