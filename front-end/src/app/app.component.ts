8e96cd5b9cc1d96e0c8962088129efd1160c8f80
Rahmasamy <rahmasamy949@gmail.com>
2024-09-22 14:24:17 +0300

    home page update

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomePageComponent } from './components/User/home-page/home-page.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
