a5a68d71ba3c9a1e5a64ddb4ad71f7c97330a744
AhmedHussein70 <106270171+AhmedHussein70@users.noreply.github.com>
2024-10-01 00:17:47 +0300

    Add files via upload

--- back-end/routes/web.php
+++ back-end/routes/web.php
@@ -1,13 +1,5 @@
 <?php
 
-// use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\CategoryControllerr;
-// use Illuminate\Support\Facades\Mail;
-// use Illuminate\Support\Facades\Log;
-// use App\Mail\TestMail;
-// use App\Mail\WelcomeMail;
-// use Illuminate\Support\Facades\Auth;
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,23 +17,4 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-//mail 
-
-// Route::get('/send', function () {
-//     Mail::to('rahmasamy20011@gmail.com')->send(new TestMail());
-//     return response('sending ...');
-//     try {
-//         Mail::to('rahmasamy949@gmail.com')->send(new WelcomeMail());
-//         return response('sending...');
-//     } catch (Exception $e) {
-//         echo $e;
-//         Log::error('Email sending failed: ' . $e->getMessage());
-//         return response('Failed to send email.', 500);
-//     }
-
-// });
-// require __DIR__ . '/auth.php';
-
-// Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//require __DIR__.'/auth.php';
